fa2163f19193dfbc7e16b63342284b0a52ce2883
Jan-Oke Mettendorf <jan-oke-mettendorf@web.de>
2021-10-10 11:23:20 +0200

    init project

---
+++ backend/src/test/java/de/neuefische/simplesportsclub/SimpleSportsClubApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.simplesportsclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSportsClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
