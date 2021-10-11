ca2d9140a97b2c4adc9b695a710961f55aa0e7a5
Jan-Oke Mettendorf <jan-oke-mettendorf@web.de>
2021-10-11 20:18:07 +0200

    Add Sidebar/Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch} from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Home from "./components/pages/Home";
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  
+  body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+  }
+  
+  html {
+    width: 100%;
+    height: 100%;
+  }
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Navigation/>
+      <Switch>
+        <Router path="/" exact>
+          <Home/>
+        </Router>
+      </Switch>
+    </Router>
+      <GlobalStyle/>
     </div>
   );
 }
