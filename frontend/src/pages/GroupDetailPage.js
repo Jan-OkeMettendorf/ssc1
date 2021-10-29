29cf2e110e06472926160e67512cbafd411f9437
Jan-Oke Mettendorf <90198828+Jan-OkeMettendorf@users.noreply.github.com>
2021-10-29 15:09:02 +0200

    add useHistory to get to detail page of selected group

---
+++ frontend/src/pages/GroupDetailPage.js
@@ -0,0 +1,25 @@
+import styled from "styled-components/macro";
+import {useParams} from "react-router-dom";
+
+
+function GroupDetailPage(){
+
+    const {id} = useParams()
+
+    return(
+        <StyledContainer>
+            asd
+        </StyledContainer>
+    )
+
+}
+
+export default GroupDetailPage;
+
+const StyledContainer = styled.div`
+  margin: 50px 50px 0 100px;
+  height: 93vh;
+  overflow: auto;
+  border: solid 1px;
+  border-radius: 5px;
+`
